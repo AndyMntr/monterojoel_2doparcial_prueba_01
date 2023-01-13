d5e4a7a3a5be5d7155402386d57419e30c16a392
Andrés Montero <jmontero7569@uta.edu.ec>
2023-01-12 20:06:50 -0500

    Commit 19h15

--- app/src/main/java/fisei/e/monterojoel_2doparcial_prueba_01/SettingsActivity.java
+++ app/src/main/java/fisei/e/monterojoel_2doparcial_prueba_01/SettingsActivity.java
@@ -1,4 +1,18 @@
 package fisei.e.monterojoel_2doparcial_prueba_01;
 
-public class SettingsActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class SettingsActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(app004.flagquizapp.R.layout.activity_config);
+        Toolbar toolbar = findViewById(app004.flagquizapp.R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
 }
--- app/src/main/java/fisei/e/monterojoel_2doparcial_prueba_01/SettingsActivityFragment.java
+++ app/src/main/java/fisei/e/monterojoel_2doparcial_prueba_01/SettingsActivityFragment.java
@@ -1,4 +1,17 @@
 package fisei.e.monterojoel_2doparcial_prueba_01;
 
-public class SettingsActivityFragment {
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SettingsActivityFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(app004.flagquizapp.R.xml.preferences);
+    }
 }
